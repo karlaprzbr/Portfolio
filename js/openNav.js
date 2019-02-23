20c175cba63ca81c528a27a745bd99e5a9f299f0
karlaprzbr <karla.perezbrugues@gmail.com>
2019-02-23 12:01:06 +0100

    add content to 'stages'

---
+++ js/openNav.js
@@ -0,0 +1,8 @@
+function openNav() {
+  var nav = document.getElementById("myTopnav");
+  if (nav.className === "topnav") {
+    nav.className += " responsive";
+  } else {
+    nav.className = "topnav";
+  }
+}
